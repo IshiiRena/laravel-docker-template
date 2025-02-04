2118caafdaeb18c880b0cb45f7ec602133fb559f
IshiiRena <ishii.rena1127@gmail.com>
2025-02-04 15:41:19 +0900

    add:データの更新を実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -37,22 +37,31 @@ class TodoController extends Controller
     }
 
     public function store(Request $request)
-{
-    $inputs = $request->all();
-    
-    
-    $this->todo->fill($inputs);
-    $this->todo->save();
+    {
+        $inputs = $request->all();
+        
+        
+        $this->todo->fill($inputs);
+        $this->todo->save();
 
-    return redirect()->route('todo.index');
-}
+        return redirect()->route('todo.index');
+    }
 
-public function edit($id)
-{
-    // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
-    $todo = $this->todo->find($id);
-    return view('todo.edit', ['todo' => $todo]);
-}
+    public function edit($id)
+    {
+        // TODO: 編集対象のレコードの情報を持つTodoモデルのインスタンスを取得
+        $todo = $this->todo->find($id);
+        return view('todo.edit', ['todo' => $todo]);
+    }
+
+    public function update(Request $request, $id) // 第1引数: リクエスト情報の取得　第2引数: ルートパラメータの取得
+    {
+        $inputs = $request->all();
+        $todo = $this->todo->find($id);
+        $todo->fill($inputs)->save();
+
+        return redirect()->route('todo.show', $todo->id);
+    }
     
 }
 
