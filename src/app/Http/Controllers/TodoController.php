d8749b57a3dcc904e6d377de4b0fc38d7c8f563e
IshiiRena <ishii.rena1127@gmail.com>
2025-02-04 17:47:51 +0900

    feat:削除機能の実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -61,5 +61,13 @@ class TodoController extends Controller
         return redirect()->route('todo.show', $todo->id);
     }
     
+    public function delete($id)
+    {
+        //dd('削除のルート実行！');
+        $todo = $this->todo->find($id);
+        $todo->delete();
+
+        return redirect()->route('todo.index');
+    }
 }
 
